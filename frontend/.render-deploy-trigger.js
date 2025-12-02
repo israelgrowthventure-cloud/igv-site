7beb02fa817a349fbb92bc7b8bc64e984399ca0c
Mickael Benmoussa <israel.growth.venture@gmail.com>
2025-12-02 15:46:37 +0200

    trigger: Add .render-deploy-trigger.js to force rebuild

---
+++ frontend/.render-deploy-trigger.js
@@ -0,0 +1,5 @@
+// Render Deploy Trigger
+// This file forces Render to detect changes and rebuild
+// Last update: 2025-12-02 16:10
+export const DEPLOY_TIMESTAMP = '2025-12-02T16:10:00Z';
+export const BUILD_VERSION = '0.1.3';
